a8ebadf3c16edb33e24662061fd8128cdceb1ac8
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 02:29:14 +0500

    User

---
+++ src/components/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const User = () => {
+    return (
+        <div>
+            <div className="avatar avatar-online">
+                <div className="w-24 rounded-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default User
