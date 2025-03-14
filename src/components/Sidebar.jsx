4d9f41d8ea09e805604bd868b058ddacc4eb8cb3
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 02:42:08 +0500

    Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -47,8 +47,13 @@ const Sidebar = () => {
                 <User />
             </div>
             {/* Footer */}
-            <div>
-                
+            <div className='flex items-center justify-between px-3 py-2'>
+                <div className="avatar">
+                    <div className="ring-primary ring-offset-base-100 w-12 rounded-full ring ring-offset-2">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <button className='btn btn-primary btn-sm px-4'>Logout</button>
             </div>
         </div>
     )
