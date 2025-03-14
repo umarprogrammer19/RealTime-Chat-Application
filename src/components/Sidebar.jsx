8d72f8a6b7d2d9a676ce5f7ae4759e57bcb6d1e5
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 02:42:42 +0500

    Gupshup

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,7 @@ import User from './User';
 const Sidebar = () => {
     return (
         <div className='max-w-[20rem] h-screen w-full flex flex-col border-r border-r-white/10'>
+            {/* Gupshup Heading */}
             <h1 className='mx-3 mt-3 rounded-lg px-2 py-1 text-primary text-xl font-semibold'>Gupshup</h1>
             {/* Search Bar */}
             <div className='p-3'>
@@ -48,11 +49,13 @@ const Sidebar = () => {
             </div>
             {/* Footer */}
             <div className='flex items-center justify-between px-3 py-2'>
+                {/* Avatar */}
                 <div className="avatar">
                     <div className="ring-primary ring-offset-base-100 w-12 rounded-full ring ring-offset-2">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
+                {/* Logout Button */}
                 <button className='btn btn-primary btn-sm px-4'>Logout</button>
             </div>
         </div>
