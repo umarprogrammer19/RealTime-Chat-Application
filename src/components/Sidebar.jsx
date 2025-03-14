3f4be91cf8921429c5c17454efe4ac10ff9864bf
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 02:23:34 +0500

    Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { IoSearch } from 'react-icons/io5';
 
 const Sidebar = () => {
     return (
-        <div></div>
+        <div className='max-w-[20rem] h-screen bg-red-600 w-full flex flex-col'>
+            <h1>Gupshup</h1>
+            {/* Search Bar */}
+            <div className='p-3'>
+                <label className="input input-bordered flex items-center gap-2">
+                    <input type="search" className='grow' required placeholder="Search" />
+                    <IoSearch />
+                </label>
+            </div>
+            {/* Rendered Users `*/}
+            <div className='h-full overflow-y-scroll'>
+
+            </div>
+            {/* Footer */}
+            <div className='h-[3rem] bg-black'>
+
+            </div>
+        </div>
     )
 }
 
