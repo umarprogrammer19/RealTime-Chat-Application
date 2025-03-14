f77fa506f64f8407e63883bc1240a0d35b33b860
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 02:15:32 +0500

    flex

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,7 +4,7 @@ import MessageContainer from '../components/MessageContainer'
 
 const Home = () => {
     return (
-        <div>
+        <div className='flex'>
             <Sidebar />
             <MessageContainer />
         </div>
