f37e2341734c0354f9516e66b8df7565ed6d8266
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 15:40:22 +0500

    Message

--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import User from './User'
+import Message from './Message'
 
 const MessageContainer = () => {
     return (
         <div className='w-full h-screen'>
+            {/* User Detail */}
             <div className='p-3 border-b border-b-white/20'>
                 <User />
             </div>
+            <div className='h-full overflow-y-auto p-4'>
+                <Message />
+                <Message />
+                <Message />
+            </div>
+            <div></div>
         </div>
     )
 }
