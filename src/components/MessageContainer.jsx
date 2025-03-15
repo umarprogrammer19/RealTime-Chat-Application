474be2cfac8cccf4eac58a458e5b822213191829
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 15:36:13 +0500

    Message Container User

--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import User from './User'
 
 const MessageContainer = () => {
     return (
-        <div></div>
+        <div className='w-full h-screen'>
+            <div className='p-3 border-b border-b-white/20'>
+                <User />
+            </div>
+        </div>
     )
 }
 
