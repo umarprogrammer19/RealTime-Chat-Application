a374dd220d57fee475aef86c769ec01de41aebc2
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-18 06:34:46 +0500

    Updation

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -3,18 +3,20 @@ import { FaUser } from "react-icons/fa";
 import { IoKeySharp } from "react-icons/io5";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
 import { loginUserThunk } from "../store/slice/users/user.thunk";
 
 const Login = () => {
 
+    // For Setting The Login Data
     const [loginData, setLoginData] = useState({
         username: "",
         password: ""
     });
 
+    // For Calling The Redux Function
     const dispatch = useDispatch();
 
+    // For Getting Values On The Change Of The Input
     const handleChange = (e) => {
         setLoginData({
             ...loginData,
@@ -22,11 +24,13 @@ const Login = () => {
         });
     };
 
+    // For Loggin In
     const handleLogin = async (event) => {
         event.preventDefault();
         dispatch(loginUserThunk(loginData));
     }
 
+    // User Interface
     return (
         <div className="flex justify-center items-center min-h-screen p-6">
             <div className="min-w-[30rem] bg-base-300 flex flex-col gap-5 p-6 rounded-lg">
