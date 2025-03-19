2e0993b9b6b592a47c7401903eb7fd37ed381786
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-19 06:42:35 +0500

    Message Slice

--- src/store/slice/messages/message.slice.js
+++ src/store/slice/messages/message.slice.js
@@ -1,7 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getMessageThunk, sendMessageThunk } from "./message.thunk";
 
 const initialState = {
     messages: null,
+    loading: false,
+    buttonLoading: false,
 }
 
 const messageSlice = createSlice({
@@ -9,6 +12,28 @@ const messageSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
+        // Send Message
+        builder.addCase(sendMessageThunk.pending, (state, action) => {
+            state.buttonLoading = true;
+        });
+        builder.addCase(sendMessageThunk.fulfilled, (state, action) => {
+            state.messages = action.payload.newMessage;
+            state.buttonLoading = false;
+        });
+        builder.addCase(sendMessageThunk.rejected, (state, action) => {
+            state.buttonLoading = true;
+        });
+        // Get Messages
+        builder.addCase(getMessageThunk.pending, (state, action) => {
+            state.loading = true;
+        });
+        builder.addCase(getMessageThunk.fulfilled, (state, action) => {
+            state.messages = action.payload.conversation;
+            state.loading = false;
+        });
+        builder.addCase(getMessageThunk.rejected, (state, action) => {
+            state.loading = true;
+        });
     }
 });
 
